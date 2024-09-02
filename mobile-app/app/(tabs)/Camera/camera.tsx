3b0d60bbaa5365ff6eed15d0d1601edb3d97f7c6
Faiz Ahmed <mfaiz-ahmed>
2024-09-02 22:44:29 +0500

    mobile app

--- mobile-app/app/(tabs)/Camera/camera.tsx
+++ mobile-app/app/(tabs)/Camera/camera.tsx
@@ -1,13 +1,15 @@
 import { useRef, useState } from "react";
-import { View, Text, Button, TouchableOpacity } from "react-native";
+import { View, Text, Button, TouchableOpacity, Image } from "react-native";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 import styles from "@/Style/Camera";
 
 export default function camera() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
-  const camera = useRef(null)
+  const [image, setImage] = useState();
+  const camera = useRef(null);
 
+  
   if (!permission) {
     // Camera permissions are still loading.
     return <View />;
@@ -30,24 +32,31 @@ export default function camera() {
   }
 
   async function takePicture() {
-    const picture = await camera.current.takePictureAsync()
-    console.log('picture' , picture);
-    
-    
+    const picture = await camera.current.takePictureAsync();
+    console.log("picture", picture);
+    setImage(picture);
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Switch</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={takePicture}>
-            <Text style={styles.text}>Click</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
+      {!image ? (
+        <CameraView ref={camera} style={styles.camera} facing={facing}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={toggleCameraFacing}
+            >
+              <Text style={styles.text}>Switch</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={takePicture}>
+              <Text style={styles.text}>Click</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      ) : (
+        <Image source={{ uri: image?.uri }}
+        style={{flex : 1}} />
+      )}
     </View>
   );
 }
